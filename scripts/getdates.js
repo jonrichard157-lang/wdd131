5fdb0a56483f14facb5c691544a464492a3fef11
João Silva <jonrichard157@gmail.com>
2026-09-08 23:28:54 -0300

    First commit

---
+++ scripts/getdates.js
@@ -0,0 +1,11 @@
+﻿// Dynamically populate the current year in the copyright notice
+const currentYearElement = document.getElementById("currentyear");
+if (currentYearElement) {
+    currentYearElement.textContent = new Date().getFullYear();
+}
+
+// Dynamically populate the last modified date of the document
+const lastModifiedElement = document.getElementById("lastModified");
+if (lastModifiedElement) {
+    lastModifiedElement.textContent = `Last Modification: ${document.lastModified}`;
+}
